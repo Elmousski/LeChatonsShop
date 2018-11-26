589f51133aed112c58f7c3e4031fb876a5f4bf2f
Elmousski <a.moussier@hotmail.fr>
2018-11-26 17:22:19 +0100

    ajout modif schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_11_26_150056) do
 
   # These are extensions that must be enabled in order to support this database
@@ -43,6 +42,5 @@ ActiveRecord::Schema.define(version: 2018_11_26_150056) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 
 end
