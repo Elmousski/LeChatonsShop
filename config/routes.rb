c92147e83d0fb9c574d75efbaa07b0b5ecea7a51
Elmousski <a.moussier@hotmail.fr>
2018-11-26 16:51:27 +0100

    front intégration template bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/' , to: 'static#index'
+  get '/assets/images/logo.png' , to: 'static#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
