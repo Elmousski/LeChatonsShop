17271d6965a5ddda6dbd2fc16e545f62a38fdcad
joelthp <alisjoel@gmail.com>
2018-11-27 14:15:30 +0100

    push mardi

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "items#index"
+  get '/' , to: 'static#index'
+  devise_for :users 
   resources :items
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
