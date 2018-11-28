8770df945b026d377157e092a24fa42c53216464
Emilie Douvier <emilie@MacBook-Air-de-Emilie.local>
2018-11-28 10:45:06 +0100

    ajout doss emilie

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   get '/' , to: 'static#index'
   devise_for :users 
   resources :items
+  get '/cart', to: 'static#cart'
+  get '/contact', to: 'static#contact'
+  get '/checkout', to: 'static#checkout'
+  get '/login_register', to: 'static#login_register'
 end
