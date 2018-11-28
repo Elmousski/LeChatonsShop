098d647aa459cfa0906a582bd2d7a52cb0ffbe9b
joelthp <alisjoel@gmail.com>
2018-11-28 23:16:16 +0100

    activestor

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,4 +4,11 @@ class StaticController < ApplicationController
   end
   def login_register
   end
+  
+private
+
+    def item_params
+      params.require(:item).permit(:title, :description, :price, :image_url, :item_image)
+    end
+
 end
