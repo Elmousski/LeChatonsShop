21def5d39b0572ae418d2330825f94d7c257e7b5
joelthp <alisjoel@gmail.com>
2018-11-29 14:42:45 +0100

    asok

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -5,10 +5,14 @@ class StaticController < ApplicationController
   def login_register
   end
   
-private
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_item
+      @item = Item.find(params[:id])
+    end
 
+    # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
       params.require(:item).permit(:title, :description, :price, :image_url, :item_image)
     end
-
 end
